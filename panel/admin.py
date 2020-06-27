47159f2ca3977331de08f8c41e6c051801891a23
berkan sems <berkansems@gmail.com>
2020-06-27 19:36:33 +0300

    rich admin panel

--- panel/admin.py
+++ panel/admin.py
@@ -6,9 +6,21 @@ from panel.models import Branch, Teacher, Courses, Student
 
 class CoursesAdmin(admin.ModelAdmin):
     list_display = ('branch','teacher','place','fee','status')
+    list_filter = ('status','fee',)
+    search_fields = ('teacher',)
+    ordering = ('fee',)
+    raw_id_fields = ('teacher',) # liste çıkarmıyor sadece id veriyor
+
+
 
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ('name','email','telephone','course')
+    list_display = ('name','email','telephone','course')# extra fields in the admin
+    list_filter = ('appStatus','course') #filters
+    search_fields = ('name','email',) #bu alanlar üzerinde search yapılacaktır
+    date_hierarchy = 'appDate' #navigate quickly through a date hierarchy.
+    ordering = ['appStatus','appDate'] #order the list by 'appStatus' and then 'appDate'
+    prepopulated_fields = {'slugName': ('name',)} # write slug of the given name automatically
+
 
 admin.site.register(Branch)
 admin.site.register(Teacher)
--- panel/models.py
+++ panel/models.py
@@ -77,6 +77,7 @@ class Student(models.Model):
         ('Rejected', 'Rejected')
     )
     name=models.CharField(max_length=50)
+    slugName = models.SlugField(max_length=200, null=True)
     email=models.EmailField()
     telephone=models.CharField(max_length=15)
     appDate=models.DateTimeField(auto_now_add=True)
